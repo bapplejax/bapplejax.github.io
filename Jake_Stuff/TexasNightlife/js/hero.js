542c22608dd534970acb0a74de40b4b42327a8c8
Jake Bapple <jake.bapple@gmail.com>
2016-07-14 13:45:40 -0500

    Changed BG Size for pictures

--- Jake_Stuff/TexasNightlife/js/hero.js
+++ Jake_Stuff/TexasNightlife/js/hero.js
@@ -4,5 +4,5 @@ $(document).ready(function(){
     
 //Build the img, then do a bit of maths to randomize load and append to a div. Add a touch off css to fade them badboys in all sexy like.
 //    $('<img class="fade-in" src="' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#banner-load');
-    $('#banner-load').css({'background': 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(img/background/' + images[Math.floor(Math.random() * images.length)] + ') 0px 0px/100% auto'});
+    $('#banner-load').css({'background': 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(img/background/' + images[Math.floor(Math.random() * images.length)] + ') 0px 0px/cover'});
 });
