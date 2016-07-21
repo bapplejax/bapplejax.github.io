77cf5c8c2fd7cec697eb16511d8b503e43b57266
Jake Bapple <jake.bapple@gmail.com>
2016-07-20 22:24:40 -0500

    Working!

--- Jake_Stuff/BoiseStateApp/js/controller/star_wars.js
+++ Jake_Stuff/BoiseStateApp/js/controller/star_wars.js
@@ -2,14 +2,29 @@ var myStarWarsApp = angular.module('myStarWarsApp', []);
 
 myStarWarsApp.controller('myStarWarsController', function ($scope, $http) {
     
-    $scope.starDatabase = [];
+    // making the data available to the markup (index.html)
+    
+    $scope.data = {
+        starDatabase: {
+            results: []
+        },
+        currentIndex: "0"        
+    };
+    
+    // helper function to filter the $index
+    
+    $scope.toInt = function toInt(paramNumber) {
+        return parseInt(paramNumber, 10);
+    }
+    
+    // making an http request to the SW api
 
     $http({
       method: 'GET',
       url: 'http://swapi.co/api/people/'
     })
     .success(function (data, status, headers, config) {
-        $scope.starDatabase = data;
+        $scope.data.starDatabase = data;
     })
     .error(function (data, status, headers, config) {
     // something went wrong :(
