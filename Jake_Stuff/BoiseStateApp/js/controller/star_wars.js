256255c4a5196f50635e62ede30a88762ae936bb
Jake Bapple <jake.bapple@gmail.com>
2016-07-27 15:47:46 -0600

    Added Styles and more Displays

--- Jake_Stuff/BoiseStateApp/js/controller/star_wars.js
+++ Jake_Stuff/BoiseStateApp/js/controller/star_wars.js
@@ -1,35 +1,34 @@
 var myStarWarsApp = angular.module('myStarWarsApp', []);
 
 myStarWarsApp.controller('myStarWarsController', function ($scope, $http) {
-    
+
     // making the data available to the markup (index.html)
-    
+
     $scope.data = {
         starDatabase: {
             results: []
         },
-        currentIndex: "0"        
+        currentIndex: "0"
     };
-    
+
     // helper function to filter the $index
-    
+
     $scope.toInt = function toInt(paramNumber) {
         return parseInt(paramNumber, 10);
     }
-    
+
     // making an http request to the SW api
 
     $http({
-      method: 'GET',
-      url: 'http://swapi.co/api/people/'
-    })
-    .success(function (data, status, headers, config) {
-        $scope.data.starDatabase = data;
-    })
-    .error(function (data, status, headers, config) {
-    // something went wrong :(
-        console.log('whoops!');
-    });
-  
+            method: 'GET',
+            url: 'http://swapi.co/api/people/'
+        })
+        .success(function (data, status, headers, config) {
+            $scope.data.starDatabase = data;
+        })
+        .error(function (data, status, headers, config) {
+            // something went wrong :(
+            console.log('whoops!');
+        });
 
 });
