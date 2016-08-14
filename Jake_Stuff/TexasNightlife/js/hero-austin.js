b089aa2e911b34f186b50bbbba892c6d4b12fd31
Jake Bapple <jake.bapple@gmail.com>
2016-08-14 14:52:36 -0600

    Put in sample ads and fixed calendar

--- Jake_Stuff/TexasNightlife/js/hero-austin.js
+++ Jake_Stuff/TexasNightlife/js/hero-austin.js
@@ -4,5 +4,5 @@ $(document).ready(function(){
     
 //Build the img, then do a bit of maths to randomize load and append to a div. Add a touch off css to fade them badboys in all sexy like.
 //    $('<img class="fade-in" src="' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#banner-load');
-    $('#banner-load').css({'background': 'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(img/background/austin/' + images[Math.floor(Math.random() * images.length)] + ') center/cover'});
+    $('#banner-load').css({'background': 'linear-gradient(rgba(0, 0, 0, 0.41), rgba(0, 0, 0, 0.72)), url(img/background/austin/' + images[Math.floor(Math.random() * images.length)] + ') center/cover'});
 });
